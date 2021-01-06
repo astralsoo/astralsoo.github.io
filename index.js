6c0f1bcea97a47f945fd6120d6ecfc64339ad3d6
astralsoo <57825401+astralsoo@users.noreply.github.com>
2021-01-06 17:43:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -157,6 +157,9 @@ $(function(){
     $(".gnb_item:nth-of-type(1)").click(function(){
       To_1st();
     })
+    $(".main").on("swipeleft", function(){
+      To_1st();
+    })
     $(".gnb_item:nth-of-type(2)").click(function(){
       To_2nd();
     })
